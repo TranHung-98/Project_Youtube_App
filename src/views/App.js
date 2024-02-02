5f0dfd22d75c17482c4a456fcef7c85acbf3d22a
TranHung <tranngochung1998@hotmail.com>
2024-02-02 12:10:51 +0700

    Changer UI

--- src/views/App.js
+++ src/views/App.js
@@ -1,22 +1,73 @@
-import logo from '../logo.svg'
 import '../style/App.scss';
-import YoutubeCloneApp from './Youtube-App';
+import React, { Component } from 'react';
+import { ShowSearchResult, YoutubeCloneApp } from './Youtube-App';
 
 
-function App() {
-  return (
-    <>
+class App extends Component {
+  state = {
+    searchTerm: '',
+    videos: [],
+    loading: false,
+    error: '',
+  };
+
+  fetchVideos = async () => {
+    const { searchTerm } = this.state;
+    const API_KEY = 'AIzaSyDqqUYo-sxHlLbWUmrF3I8zTdswuRVpIKw';
+    try {
+      this.setState({ loading: true, error: '' });
+
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${API_KEY}`
+      );
+      const data = await response.json();
+
+      if (data.error) {
+        throw new Error(data.error.message);
+      }
+
+      this.setState({ videos: data.items, loading: false });
+    } catch (error) {
+      this.setState({ error: 'Error fetching videos', loading: false });
+    }
+  };
+
+
+  handleSearch = () => {
+    const { searchTerm } = this.state;
+
+    if (searchTerm.trim().length >= 1) {
+      this.fetchVideos();
+    }
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      this.handleSearch();
+    }
+  };
+
+  render() {
+    const { videos, loading, error } = this.state;
+
+    return (
       <div className="App">
+        <YoutubeCloneApp
+          searchTerm={this.state.searchTerm}
+          onSearch={this.handleSearch}
+          onInputChange={this.handleInputChange}
+          onKeyDown={this.handleKeyDown}
+        />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='m-3'>
-            Welcome To Youtube App
-          </h1>
-          <YoutubeCloneApp />
+          <ShowSearchResult videos={videos} loading={loading} error={error} />
         </header>
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
